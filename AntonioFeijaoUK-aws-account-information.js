e7de6df0b48e9dae268921fde43b8a0abdc67b58
Antonio Feijao UK <2548084+AntonioFeijaoUK@users.noreply.github.com>
2024-01-05 17:49:55 +0000

    Update AntonioFeijaoUK-aws-account-information.js

--- AntonioFeijaoUK-aws-account-information.js
+++ AntonioFeijaoUK-aws-account-information.js
@@ -1,68 +1,48 @@
 // ==UserScript==
 // @name         AntonioFeijaoUK-aws-account-information
 // @namespace    https://*.console.aws.amazon.com/*
-// @version      3.5
-// @description  This script logs on the browser console the AWS account ID, the username (role) and the account name that you are currently logged in.
+// @version      3.6
+// @description  This script logs on the browser console the AWS account ID, the username (role) and account name that you are currently logged in.
 // @author       AntonioFeijaoUK (https://antonio.cloud/
 // @updateURL    https://raw.githubusercontent.com/AntonioFeijaoUK/aws-tampermonkey-scripts/main/AntonioFeijaoUK-aws-account-information.js
 // @downloadURL    https://raw.githubusercontent.com/AntonioFeijaoUK/aws-tampermonkey-scripts/main/AntonioFeijaoUK-aws-account-information.js
 // @match        https://*.console.aws.amazon.com/*
 // @icon         https://console.aws.amazon.com/favicon.ico?aws.amazon.com
+// @grant        GM_addStyle
 // ==/UserScript==
 
-(function () {
-  "use strict";
+(window.addEventListener('load', function AddBanner() {
+    "use strict";
+    // Your code here...
+    //console.log("Something hello world from tampermonkey");
 
-  // Your code here...
-  //console.log("Something hello world from tampermonkey");
+    function fullDecode(input) {
+        let decoded = decodeURIComponent(input);
+        return decoded == input ? decoded : fullDecode(decoded);
+    };
 
-  function fullDecode(input) {
-    let decoded = decodeURIComponent(input);
-    return decoded == input ? decoded : fullDecode(decoded);
-  }
+    let userInfo = document.cookie.replace(
+        /(?:(?:^|.*;\s*)aws-userInfo\s*\=\s*([^;]*).*$)|^.*$/,
+        "$1"
+    );
 
-  let userInfo = document.cookie.replace(
-    /(?:(?:^|.*;\s*)aws-userInfo\s*\=\s*([^;]*).*$)|^.*$/,
-    "$1"
-  );
-
-  //alert(JSON.stringify(JSON.parse(fullDecode(userInfo)), null, 4));
-  //console.log(JSON.stringify(JSON.parse(fullDecode(userInfo)), null, 4));
-
-  //let AntonioAccountInfo = JSON.stringify(
-  //    JSON.parse(fullDecode(userInfo)), null, 4 );
-
-  let AntonioAccountInfo = JSON.parse(fullDecode(userInfo));
-
-  //let user_name = AntonioAccountInfo.username.split('/');
-  //let user_name = AntonioAccountInfo.username.split("_")[1];
-  //console.log("     user name : " + user_name);
+    let AccountInfo = JSON.parse(fullDecode(userInfo));
 
     function getUsername(user_name) {
-      if (AntonioAccountInfo.username.split("_")[1] == null) {
-          let user_name = '[<span style="background-color:orange;color:white;"><b>' + AntonioAccountInfo.username.split("/")[0] + '</b></span>]';
-          return user_name;
-      } else {
-          let user_name = '[' + AntonioAccountInfo.username.split("_")[1] + ']';
-          return user_name;
-      }
+        if (AccountInfo.username.split("_")[1] == null) {
+            let user_name = '[<span style="background-color:orange;color:white;"><b>' + AccountInfo.username.split("/")[0] + '</b></span>]';
+            return user_name;
+        } else {
+            let user_name = '[' + AccountInfo.username.split("_")[1] + ']';
+            return user_name;
+        }
     }
 
-  let user_name = getUsername(AntonioAccountInfo);
+    let user_name = getUsername(AccountInfo);
+    let account_name = AccountInfo.issuer.split(" ")[1].split("/")[0];
+    let account_number = AccountInfo.alias;
 
-
-  let account_name = AntonioAccountInfo.issuer.split(" ")[1].split("/")[0];
-  let account_number = AntonioAccountInfo.alias;
-
-  //let region = decodeURIComponent(document.cookie).split(";")[6].split("=")[1];
-  let region = decodeURIComponent(document.cookie.replace(/(?:(?:^|.*;\s*)noflush_Region\s*\=\s*([^;]*).*$)|^.*$/,"$1"));
-
-
-  ///let assume_role_account_number = decodeURIComponent(document.cookie).split(";")[17].split(",")[1].split('"')[3];
-  //let assume_role_account_name   = decodeURIComponent(document.cookie).split(";")[19].split(",")[3].split(":")[1];
-
-
-window.addEventListener("load", function addBanner() {
+    let region = decodeURIComponent(document.cookie.replace(/(?:(?:^|.*;\s*)noflush_Region\s*\=\s*([^;]*).*$)|^.*$/, "$1"));
 
     if (account_name) {
         console.log("account number : " + account_number);
@@ -70,22 +50,15 @@ window.addEventListener("load", function addBanner() {
         console.log("  account name : " + account_name);
         console.log("        region : " + region);
 
-   //console.log("assume_role_account_number : " + assume_role_account_number);
-   // console.log("assume_role_account_name   : " + assume_role_account_name);
-
-
-    // --------------------------------------------------------------------------
-    // div code's credits goes to @barney_parker (https://twitter.com/barney_parker)
-
         var div = document.createElement("div");
-    //div.className = "antonio-banner";
-    //        div.style['background-color'] = '#232f3e';
-    //        div.style.color = '#f8991d';
-    //        div.style['font-size'] = 'medium';
-    //        div.style.hr = 'border: 2px solid green; border-radius: 5px;';
-    //        div.style.padding = '0.25em';
+        //div.className = "additional-banner";
+        //div.style['background-color'] = '#232f3e';
+        //div.style.color = '#f8991d';
+        //div.style['font-size'] = 'medium';
+        //div.style.hr = 'border: 2px solid green; border-radius: 5px;';
+        //div.style.padding = '0.25em';
 
-    //div.style.position = "absolute";
+        //div.style.position = "absolute";
         div.style.height = "40px";
         div.style["font-size"] = "16px";
         div.style["line-height"] = "40px";
@@ -104,25 +77,20 @@ window.addEventListener("load", function addBanner() {
 
         div.innerHTML = '<span>' + user_name + ' - ' + region + ' - ' + account_number + ' - <strong>' + account_name + '</strong></span>';
 
-        console.log("logging the element 1 if exist: " + document.getElementById("awsc-navigation-container"));
-        console.log("logging the element 2 if exist: " + document.getElementById("awsc-nav-header"));
-        console.log("logging the element 3 if exist: " + document.getElementById("consoleNavHeader"));
-        console.log("logging the element 4 if exist: " + document.getElementById("b"));
+        let element = document.getElementById("consoleNavHeader");
 
-    //document.body.insertBefore(div,document.body.childNodes[0]);
+        if (element) {
+            console.log("logging the element 1 if exist: " + document.getElementById("awsc-navigation-container"));
+            console.log("logging the element 2 if exist: " + document.getElementById("awsc-nav-header"));
+            console.log("logging the element 3 if exist: " + document.getElementById("consoleNavHeader"));
+            console.log("logging the element 4 if exist: " + document.getElementById("b"));
 
-    //parent.prepend(div);
+            //document.body.insertBefore(div,document.body.childNodes[0]);
 
-    //const parent = document.getElementById("awsc-navigation-container");
-        const parent = document.getElementById("consoleNavHeader");
+            //const parent = document.getElementById("consoleNavHeader");
+            const parent = element;
+            parent.prepend(div);
+        };
 
-    //console.log("loging parent " + parent);
-
-        parent.prepend(div);
-
-
-    }
-});
-
-
-})();
+    };
+}), false);
