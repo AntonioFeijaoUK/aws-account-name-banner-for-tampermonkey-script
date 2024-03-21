6e571391d370b957557ed0272bddfe2c9f6ee58d
Antonio Feijao UK <2548084+AntonioFeijaoUK@users.noreply.github.com>
2024-03-21 15:21:16 +0000

    updated the script to deal with assumed roles but also the removal of account name that is no longer in the cookie.

--- AntonioFeijaoUK-aws-account-information.js
+++ AntonioFeijaoUK-aws-account-information.js
@@ -8,8 +8,11 @@
 // @downloadURL    https://raw.githubusercontent.com/AntonioFeijaoUK/aws-tampermonkey-scripts/main/AntonioFeijaoUK-aws-account-information.js
 // @match        https://*.console.aws.amazon.com/*
 // @icon         https://console.aws.amazon.com/favicon.ico?aws.amazon.com
-// @grant        GM_addStyle
 // ==/UserScript==
+
+// // @grant        GM_addStyle
+
+
 
 (window.addEventListener('load', function AddBanner() {
     "use strict";
@@ -21,34 +24,48 @@
         return decoded == input ? decoded : fullDecode(decoded);
     };
 
-    let userInfo = document.cookie.replace(
-        /(?:(?:^|.*;\s*)aws-userInfo\s*\=\s*([^;]*).*$)|^.*$/,
-        "$1"
-    );
+    let userInfo = document.cookie.replace(/(?:(?:^|.*;\s*)aws-userInfo\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 
     let AccountInfo = JSON.parse(fullDecode(userInfo));
 
     function getUsername(user_name) {
         if (AccountInfo.username.split("_")[1] == null) {
-            let user_name = '[<span style="background-color:orange;color:white;"><b>' + AccountInfo.username.split("/")[0] + '</b></span>]';
+            let user_name = AccountInfo.username.split("/")[2];
             return user_name;
         } else {
-            let user_name = '[' + AccountInfo.username.split("_")[1] + ']';
+            let user_name = AccountInfo.username.split("/")[2];
             return user_name;
         }
     }
 
-    let user_name = getUsername(AccountInfo);
-    let account_name = AccountInfo.issuer.split(" ")[1].split("/")[0];
-    let account_number = AccountInfo.alias;
+
+    // getting the permissions
+    function getPermissions(AccountInfo) {
+        if (AccountInfo.username.split("_")[1] == null) {
+            let permissions = AccountInfo.username;
+            return permissions;
+        } else {
+            let permissions = AccountInfo.username.split("_")[1];
+            return permissions;
+        }
+    }
+
 
     let region = decodeURIComponent(document.cookie.replace(/(?:(?:^|.*;\s*)noflush_Region\s*\=\s*([^;]*).*$)|^.*$/, "$1"));
+    let permissions = getPermissions(AccountInfo);
+    let account_number = AccountInfo.alias;
+    //let account_name = AccountInfo.issuer.split(" ")[1].split("/")[0];
+    let user_name = getUsername(AccountInfo);
+    let account_name = AccountInfo.username;
+
 
     if (account_name) {
+        console.log("        region : " + region);
+        console.log("   permissions : " + permissions);
         console.log("account number : " + account_number);
         console.log("     user name : " + user_name);
         console.log("  account name : " + account_name);
-        console.log("        region : " + region);
+
 
         var div = document.createElement("div");
         //div.className = "additional-banner";
@@ -74,8 +91,8 @@
         div.style.width = "100%";
         div.style["z-index"] = "10000";
         div.style.top = "0";
-
-        div.innerHTML = '<span>' + user_name + ' - ' + region + ' - ' + account_number + ' - <strong>' + account_name + '</strong></span>';
+        // ## 2024-03-21 - account_name is not working at the moment, account name is no longer part of the document.cookie....
+        div.innerHTML = '<span>[' + region + ' - <strong>' + permissions + '</strong> - ' + account_number + ' - ' + user_name + ']</span>';
 
         let element = document.getElementById("consoleNavHeader");
 
