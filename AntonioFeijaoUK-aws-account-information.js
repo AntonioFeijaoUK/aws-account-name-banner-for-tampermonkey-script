1893f096bb117816744b3efc0887ab6b0219b76c
Antonio Feijao UK <2548084+AntonioFeijaoUK@users.noreply.github.com>
2024-07-16 10:12:46 +0100

    Update AntonioFeijaoUK-aws-account-information.js

--- AntonioFeijaoUK-aws-account-information.js
+++ AntonioFeijaoUK-aws-account-information.js
@@ -1,113 +1,135 @@
 // ==UserScript==
 // @name         AntonioFeijaoUK-aws-account-information
 // @namespace    https://*.console.aws.amazon.com/*
-// @version      3.6
+// @version      4.1
 // @description  This script logs on the browser console the AWS account ID, the username (role) and account name that you are currently logged in.
-// @author       AntonioFeijaoUK (https://antonio.cloud/
+// @author       AntonioFeijaoUK (https://antonio.cloud/)
 // @updateURL    https://raw.githubusercontent.com/AntonioFeijaoUK/aws-tampermonkey-scripts/main/AntonioFeijaoUK-aws-account-information.js
-// @downloadURL    https://raw.githubusercontent.com/AntonioFeijaoUK/aws-tampermonkey-scripts/main/AntonioFeijaoUK-aws-account-information.js
+// @downloadURL  https://raw.githubusercontent.com/AntonioFeijaoUK/aws-tampermonkey-scripts/main/AntonioFeijaoUK-aws-account-information.js
 // @match        https://*.console.aws.amazon.com/*
 // @icon         https://console.aws.amazon.com/favicon.ico?aws.amazon.com
 // ==/UserScript==
 
-// // @grant        GM_addStyle
+(function() {
+    'use strict';
 
-
-
-(window.addEventListener('load', function AddBanner() {
-    "use strict";
-    // Your code here...
-    //console.log("Something hello world from tampermonkey");
-
+    // Function to decode URI component recursively
     function fullDecode(input) {
-        let decoded = decodeURIComponent(input);
-        return decoded == input ? decoded : fullDecode(decoded);
-    };
+        try {
+            let decoded = decodeURIComponent(input);
+            return decoded == input ? decoded : fullDecode(decoded);
+        } catch (error) {
+            console.error('Error decoding input:', error);
+            return input; // Fallback to original input on error
+        }
+    }
 
+    // Retrieve AWS user information from cookies
     let userInfo = document.cookie.replace(/(?:(?:^|.*;\s*)aws-userInfo\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    let AccountInfo;
+    try {
+        AccountInfo = JSON.parse(fullDecode(userInfo));
+    } catch (error) {
+        console.error('Error parsing userInfo JSON:', error);
+        return; // Exit early if userInfo cannot be parsed
+    }
 
-    let AccountInfo = JSON.parse(fullDecode(userInfo));
-
-    function getUsername(user_name) {
-        if (AccountInfo.username.split("_")[1] == null) {
-            let user_name = AccountInfo.username.split("/")[2];
-            return user_name;
-        } else {
-            let user_name = AccountInfo.username.split("/")[2];
-            return user_name;
+    // Function to extract username from AccountInfo
+    function getUsername() {
+        try {
+            if (AccountInfo.username.split("_")[1] == null) {
+                return AccountInfo.username.split("/")[2];
+            } else {
+                return AccountInfo.username.split("_")[1];
+            }
+        } catch (error) {
+            console.error('Error extracting username:', error);
+            return ''; // Return empty string on error
         }
     }
 
-
-    // getting the permissions
-    function getPermissions(AccountInfo) {
-        if (AccountInfo.username.split("_")[1] == null) {
-            let permissions = AccountInfo.username;
-            return permissions;
-        } else {
-            let permissions = AccountInfo.username.split("_")[1];
-            return permissions;
+    // Function to extract permissions from AccountInfo
+    function getPermissions() {
+        try {
+            if (AccountInfo.username.split("_")[1] == null) {
+                return AccountInfo.username;
+            } else {
+                return AccountInfo.username.split("_")[1];
+            }
+        } catch (error) {
+            console.error('Error extracting permissions:', error);
+            return ''; // Return empty string on error
         }
     }
 
+    // Get the accounts list, ready for cost dictionary.
+    // Read the list of account from Payer account.
+    //
+    //  aws organizations list-accounts > ./results/00-list-of-accounts-in-org.txt
+    //  cat ./results/00-list-of-accounts-in-org.txt | jq -r '.Accounts[] | "\(.Name)\t \(.Id);" ' | tr -d '^ ' | tr ' ' '_' | sort --ignore-case | awk '{print $2 "\t" $1 }' | sed s/"^"/"'"/g | sed s/"\$"/"',"/g | sed s/';\t'/"': '"/g
+    //
 
-    let region = decodeURIComponent(document.cookie.replace(/(?:(?:^|.*;\s*)noflush_Region\s*\=\s*([^;]*).*$)|^.*$/, "$1"));
-    let permissions = getPermissions(AccountInfo);
-    let account_number = AccountInfo.alias;
-    //let account_name = AccountInfo.issuer.split(" ")[1].split("/")[0];
-    let user_name = getUsername(AccountInfo);
-    let account_name = AccountInfo.username;
-
-
-    if (account_name) {
-        console.log("        region : " + region);
-        console.log("   permissions : " + permissions);
-        console.log("account number : " + account_number);
-        console.log("     user name : " + user_name);
-        console.log("  account name : " + account_name);
-
-
-        var div = document.createElement("div");
-        //div.className = "additional-banner";
-        //div.style['background-color'] = '#232f3e';
-        //div.style.color = '#f8991d';
-        //div.style['font-size'] = 'medium';
-        //div.style.hr = 'border: 2px solid green; border-radius: 5px;';
-        //div.style.padding = '0.25em';
-
-        //div.style.position = "absolute";
-        div.style.height = "40px";
-        div.style["font-size"] = "16px";
-        div.style["line-height"] = "40px";
-        div.style["padding-left"] = "30px";
-        div.style["padding-right"] = "30px";
-        div.style.overflow = "hidden";
-        div.style["font-weight"] = "400";
-        div.style.color = "orange"; // '#f8991d'; //div.style.color = '#f6f6f6';
-        div.style.background = "#232f3e";
-        div.style["border-bottom"] = "1px solid orange";
-        div.style.display = 'flex';
-        div.style["justify-content"] = "space-between";
-        div.style.width = "100%";
-        div.style["z-index"] = "10000";
-        div.style.top = "0";
-        // ## 2024-03-21 - account_name is not working at the moment, account name is no longer part of the document.cookie....
-        div.innerHTML = '<span>[' + region + ' - <strong>' + permissions + '</strong> - ' + account_number + ' - ' + user_name + ']</span>';
-
-        let element = document.getElementById("consoleNavHeader");
-
-        if (element) {
-            console.log("logging the element 1 if exist: " + document.getElementById("awsc-navigation-container"));
-            console.log("logging the element 2 if exist: " + document.getElementById("awsc-nav-header"));
-            console.log("logging the element 3 if exist: " + document.getElementById("consoleNavHeader"));
-            console.log("logging the element 4 if exist: " + document.getElementById("b"));
-
-            //document.body.insertBefore(div,document.body.childNodes[0]);
-
-            //const parent = document.getElementById("consoleNavHeader");
-            const parent = element;
-            parent.prepend(div);
-        };
-
+    // Dictionary to map account IDs to account names
+    const dictionary = {
+        'accountid1': 'account_name1',
+        'accountid2': 'account_name2',
+        'accountid3': 'account_name3',
+        'accountid4': 'account_name4',
+        'accountid5': 'account_name5',
     };
-}), false);
+
+    // Retrieve region from cookies
+    let region = decodeURIComponent(document.cookie.replace(/(?:(?:^|.*;\s*)noflush_Region\s*\=\s*([^;]*).*$)|^.*$/, "$1"));
+
+    // Retrieve permissions and account number from AccountInfo
+    let permissions = getPermissions();
+    let account_number = AccountInfo.alias;
+
+    // Retrieve account name from dictionary based on account number
+    let account_name = dictionary[account_number] || 'no-name-in-list';
+
+    // Retrieve username
+    let user_name = getUsername();
+
+    // Logging account information to console
+    console.log("        region : " + region);
+    console.log("   permissions : " + permissions);
+    console.log("account number : " + account_number);
+    console.log("     user name : " + user_name);
+    console.log("  account name : " + account_name);
+
+    // Create the banner div element
+    var div = document.createElement("div");
+    div.className = "additional-banner";
+    div.style.height = "40px";
+    div.style.fontSize = "16px";
+    div.style.lineHeight = "40px";
+    div.style.paddingLeft = "30px";
+    div.style.paddingRight = "30px";
+    div.style.overflow = "hidden";
+    div.style.fontWeight = "400";
+    div.style.color = "orange"; // Adjust color as needed
+    div.style.background = "#232f3e"; // Adjust background color as needed
+    div.style.borderBottom = "1px solid orange"; // Adjust border as needed
+    div.style.display = 'flex';
+    div.style.justifyContent = "space-between";
+    div.style.width = "100%";
+    div.style.zIndex = "10000";
+    div.style.top = "0";
+    div.style.left = "0"; // Position from the left edge
+    div.style.right = "0"; // Position from the right edge
+
+    // Set banner content
+    div.innerHTML = '<span>[' + region + ' - <strong>' + permissions + '</strong> - ' + account_name + ' ' + account_number + ' ' + user_name + ']</span>';
+
+    // Insert the banner into the AWS Console header
+    let headerElement = document.getElementById("consoleNavHeader");
+    if (headerElement) {
+        headerElement.prepend(div);
+    } else {
+        console.warn("AWS Console header element not found. Banner may not be positioned correctly.");
+        // Fallback: Insert banner at the top of the body if header element is not found
+        document.body.insertBefore(div, document.body.firstChild);
+    }
+
+})();
